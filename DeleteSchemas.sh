c6778229900f3c7814ad6a986ced7e77446578f4
Jos3lu <jlsierrabenito@gmail.com>
2023-10-13 19:09:25 +0200

    Update DeleteSchemas.sh

--- DeleteSchemas.sh
+++ DeleteSchemas.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-CREATORS_VALUE_SOFT=$(curl --silent -X DELETE -u client:client http://localhost:8081/subjects/creators-value)
 CREATORS_KEY_SOFT=$(curl --silent -X DELETE -u client:client http://localhost:8081/subjects/creators-key)
-CREATORS_VALUE_HARD=$(curl --silent -X DELETE -u client:client http://localhost:8081/subjects/creators-key?permanent=true)
-CREATORS_KEY_HARD=$(curl --silent -X DELETE -u client:client http://localhost:8081/subjects/creators-value?permanent=true)
-echo $CREATORS_VALUE_SOFT
+CREATORS_VALUE_SOFT=$(curl --silent -X DELETE -u client:client http://localhost:8081/subjects/creators-value)
+CREATORS_KEY_HARD=$(curl --silent -X DELETE -u client:client http://localhost:8081/subjects/creators-key?permanent=true)
+CREATORS_VALUE_HARD=$(curl --silent -X DELETE -u client:client http://localhost:8081/subjects/creators-value?permanent=true)
 echo $CREATORS_KEY_SOFT
+echo $CREATORS_VALUE_SOFT
 echo $CREATORS_VALUE_HARD
 echo $CREATORS_KEY_HARD
+
+VIDEOS_KEY_SOFT=$(curl --silent -X DELETE -u client:client http://localhost:8081/subjects/videos-key)
+VIDEOS_VALUE_SOFT=$(curl --silent -X DELETE -u client:client http://localhost:8081/subjects/videos-value)
+VIDEOS_KEY_HARD=$(curl --silent -X DELETE -u client:client http://localhost:8081/subjects/videos-key?permanent=true)
+VIDEOS_VALUE_HARD=$(curl --silent -X DELETE -u client:client http://localhost:8081/subjects/videos-value?permanent=true)
+echo $VIDEOS_KEY_SOFT
+echo $VIDEOS_VALUE_SOFT
+echo $VIDEOS_KEY_HARD
+echo $VIDEOS_VALUE_HARD
